cd28f94ab2b6b66ba37d3c0fc5d20909d729d956
fren_gor <goro@frengor.com>
2021-05-03 21:50:57 +0200

    Remove old code

--- src/main/java/com/fren_gor/invManagementPlugin/api/gui/BlockGuiInteractions.java
+++
@@ -1,8 +0,0 @@
-package com.fren_gor.invManagementPlugin.api.gui;
-
-/**
- * Implement alongside {@link org.bukkit.inventory.InventoryHolder} to block all the possible interactions with both top and bottom<br>
- * inventories of a {@link org.bukkit.inventory.InventoryView}.
- */
-public interface BlockGuiInteractions {
-}
--- src/main/java/com/fren_gor/invManagementPlugin/api/gui/BlockTopGuiInteractions.java
+++
@@ -1,8 +0,0 @@
-package com.fren_gor.invManagementPlugin.api.gui;
-
-/**
- * Implement alongside {@link org.bukkit.inventory.InventoryHolder} to block all the possible interactions with the top<br>
- * inventory of a {@link org.bukkit.inventory.InventoryView}.
- */
-public interface BlockTopGuiInteractions {
-}
