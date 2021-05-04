d04e79f905592247e8d6ed41a0b86b6204400a39
fren_gor <goro@frengor.com>
2021-05-04 23:55:02 +0200

    Update EmptyGui.java

--- Common/src/main/java/com/fren_gor/invManagementPlugin/gui/EmptyGui.java
+++ Common/src/main/java/com/fren_gor/invManagementPlugin/gui/EmptyGui.java
@@ -6,43 +6,32 @@ import lombok.Getter;
 import org.apache.commons.lang.Validate;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.jetbrains.annotations.Range;
 
 import java.util.Objects;
-import java.util.function.Consumer;
 
-public abstract class EmptyGui implements InventoryHolder, ClickListener, CloseListener {
+public class EmptyGui implements InventoryHolder, ClickListener, CloseListener {
 
     @Getter
-    @NotNull
-    private final Player player;
+    protected final Plugin plugin;
     @Getter
-    @NotNull
-    private final String title;
+    protected final Player player;
     @Getter
-    @Range(from = 9, to = 54)
-    private final int size;
-    @Getter
-    @Nullable
-    private final Consumer<InventoryCloseEvent> onClose;
+    protected final String title;
+    protected final int size;
 
     public EmptyGui(@NotNull Plugin plugin, @NotNull Player player, @NotNull String title, int size) {
-        this(plugin, player, title, size, null);
-    }
-
-    public EmptyGui(@NotNull Plugin plugin, @NotNull Player player, @NotNull String title, int size, @Nullable Consumer<InventoryCloseEvent> onClose) {
         Validate.notNull(plugin, "Plugin is null.");
         Validate.isTrue(plugin.isEnabled(), "Plugin is not enabled.");
         Validate.isTrue(size > 0 && size <= 54 && size % 9 == 0, "Invalid size");
+        this.plugin = plugin;
         this.title = Objects.requireNonNull(title);
         this.player = Objects.requireNonNull(player);
-        this.onClose = onClose;
         this.size = size;
         Bukkit.getScheduler().callSyncMethod(plugin, () -> player.openInventory(getInventory()));
     }
@@ -51,6 +40,18 @@ public abstract class EmptyGui implements InventoryHolder, ClickListener, CloseL
     @NotNull
     public Inventory getInventory() {
         return Bukkit.createInventory(this, size, title);
+    }
+
+    @Override
+    public void onClick(@NotNull InventoryClickEvent event) {
+    }
+
+    @Override
+    public void onClose(@NotNull InventoryCloseEvent event) {
+    }
+
+    public int getSize() {
+        return size;
     }
 
 }
